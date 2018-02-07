9fa8b3740e8e08e328bdcf85b4cf9958707ae9df
宋路 <song.l@haihangyun.com>
2018-02-07 22:13:58 +0800

    faker local very easy

--- config/app.php
+++ config/app.php
@@ -67,6 +67,9 @@ return [
 
     'timezone' => 'UTC',
 
+    //faker本地化
+    'faker_locale' => 'zh_CN',
+
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
